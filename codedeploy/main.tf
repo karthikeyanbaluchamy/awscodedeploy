c63993bf032b5acbdda87dbf7b979f0cb6fad1ad
karthikeyan <55027856+karthikeyanbaluchamy@users.noreply.github.com>
2026-04-19 12:48:06 -0400

    Update main.tf

--- codedeploy/main.tf
+++ codedeploy/main.tf
@@ -1,8 +1,7 @@
 
 provider "aws" {
-  region = "us-east-1"
-    access_key = "AKIA5FTY7BG2KXSHH5EG"
-  secret_key = "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"
+	  region = "us-east-1
+  
 }
 
 module "ecr" {
